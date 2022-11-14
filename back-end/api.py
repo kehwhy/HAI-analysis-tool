b8e6cd200a72f3696cb3e40165b060312d63809c
kyjauna <kyjauna.marshall@mail.mcgill.ca>
2022-11-14 15:47:54 -0500

    test

--- back-end/api.py
+++ back-end/api.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
-import os
-from autogluon.tabular import TabularDataset, TabularPredictor
+from autogluon.tabular import TabularPredictor
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import json
+import os
 
 app = Flask(__name__)
 
@@ -21,6 +21,12 @@ def generate_model_from_input():
     label = request.form['label']
     if uploaded_file.filename != '' and label != '':
         df = pd.read_csv(uploaded_file)
+        # make all headers lowercase
+        df.columns = df.columns.str.lower()
+        # make client label input all lowercase
+        label = label.lower()
+        # save uploaded csv to workfile
+        df.to_csv('backend/workfile.csv', encoding='utf-8', index=False)
         train_data, test_data = train_test_split(df, test_size=0.2)
         save_path = 'agModels-predictClass'  # specifies folder to store trained models
         predictor = TabularPredictor(label=label, path=save_path).fit(
@@ -35,10 +41,26 @@ def generate_model_from_input():
         my_json_string = json.dumps({'Model': predictor.get_model_best(), 'Evaluation': eval, 'ModelPath': save_path})
         return(my_json_string)
 
-@app.route('/get_model_bias', methods=['POST'])
-def get_model_bias():
-    path = request.form['save_path']
-    # get the model bias file
+# @app.route('/remove_existing_model', methods=['POST'])
+# def remove_existing_model():
+#     # remove existing model from path
+#     path = request.form['save_path']
+#     os.remove(path)
+#     return("200")
+
+# @app.route('/generate_default_data', methods=['POST'])
+# def generate_default_data():
+#     # get the model
+#     path = request.form['save_path']
+#     predictor = TabularPredictor.load(path)
+#     y_pred = predictor.predict(test_data_nolabel)
+
+# @app.route('/get_model_bias', methods=['POST'])
+# def get_model_bias():
+#     # get the model
+#     path = request.form['save_path']
+#     predictor = TabularPredictor.load(path)
+    
 
 
 if (__name__ == "__main__"):
